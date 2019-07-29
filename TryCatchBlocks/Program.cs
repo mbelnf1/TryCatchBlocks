68a009b96634b09a94a39f273917a19a3cf2e5ea
mbelnf1 <mbeln1992@gmail.com>
2019-07-29 15:49:49 -0400

    Add project files.

---
+++ TryCatchBlocks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TryCatchBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ageText = "";
+            int age = 0;
+
+            Console.Write("What is your age: ");
+            ageText = Console.ReadLine();
+
+            try
+            {
+                age = int.Parse(ageText);
+            }
+                catch (FormatException ex)
+            {
+                Console.WriteLine("You did not enter a valid number.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected exception occurred.");
+            }
+            
+
+            Console.WriteLine("Your age is {0}", age);
+
+            Console.Read();
+        }
+    }
+}
